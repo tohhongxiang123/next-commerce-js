a07b5193eb7eaa8bf716d84af72f9c799a8cc348
tohhongxiang <tohhongxiang@gmail.com>
2021-08-26 02:39:12 +0800

    chore: Update css for product and productlist

--- components/DisplayProduct.tsx
+++ components/DisplayProduct.tsx
@@ -1,10 +1,21 @@
 import { Product } from "@chec/commerce.js/types/product";
+import Link from 'next/link'
 
-export default function DisplayProduct({ id, permalink, name, description, price }: Product) {
+const MAX_SHOWN_CATEGORIES = 5
+export default function DisplayProduct({ id, permalink, name, description, price, categories, ...props }: Product) {
+    const coverImage = props.assets[0]
     return (
-        <div>
-            <p className="font-semibold">{name}</p>
-            <p>{price.formatted_with_symbol}</p>
+        <div className="p-4 w-full h-full">
+            <Link href={`/products/${permalink}`}>
+                <a className="block relative h-48 rounded overflow-hidden">
+                    <img className="object-cover object-center w-full h-full block" src={coverImage ? coverImage.url : '/no_image_placeholder.svg'} alt={coverImage ? coverImage.filename : name} />
+                </a>
+            </Link>
+            <div className="mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{categories.slice(0, MAX_SHOWN_CATEGORIES).map(category => category.name.toUpperCase()).join(', ')}</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium"><Link href={`/products/${permalink}`}><a>{name}</a></Link></h2>
+                <p className="mt-1">{price.formatted_with_symbol}</p>
+            </div>
         </div>
     )
 }
--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -10,9 +10,9 @@ interface ProductListProps {
 export default function ProductList({ products = [] }: ProductListProps) {
     return (
         <div>
-            <ul>
+            <ul className="flex flex-wrap gap-8 w-full">
                 {products.map(product => (
-                    <li key={product.id}>
+                    <li key={product.id} className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-sm rounded-sm">
                         <Link href={`/products/${product.permalink}`} passHref>
                             <a>
                                 <DisplayProduct {...product} />
